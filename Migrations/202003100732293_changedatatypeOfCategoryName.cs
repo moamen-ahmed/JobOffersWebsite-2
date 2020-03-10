e42d55b6c5dd4e82876c8aab73fceb98f1de09d9
moamen ahmed lotfy mahmoud <eng_moemenahmed2010@yahoo.com>
2020-03-10 11:07:33 +0200

    Add project files.

---
+++ Migrations/202003100732293_changedatatypeOfCategoryName.cs
@@ -0,0 +1,18 @@
+﻿namespace JobOffersWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedatatypeOfCategoryName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryName", c => c.Int(nullable: false));
+        }
+    }
+}
